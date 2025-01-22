ce9772ad4e86db8d610bcba7d0d1d3c28bacad93
Arshad Kazi <kaziarshad97@gmail.com>
2025-01-22 16:42:59 +0000

    pretraining pipeline

--- source/DataLoaders.py
+++ source/DataLoaders.py
@@ -1,6 +1,7 @@
 import torch 
 import numpy as np
 import pickle
+from torch.utils.data import DataLoader as TorchDataLoader
 
 class DataLoader:
     def __init__(self, data_path, frame_threshold): 
@@ -21,17 +22,18 @@ class DataLoader:
         self.data = new_data
         return self.data
 
-
     def main(self): 
         data = self.load_data()
         data = self.prefiltering()
         return data
 
 
+
 class Dataset(torch.utils.data.Dataset):
-    def __init__(self, data_path, frame_threshold, max_frames =10): 
+    def __init__(self, data_path, frame_threshold, max_frames =10, if_train = True): 
         self.data = DataLoader(data_path, frame_threshold).main()
         self.max_frames = max_frames
+        self.if_train = if_train
 
     def split_video(self, video): 
         max_splits = video.shape[1] // self.max_frames
@@ -43,14 +45,37 @@ class Dataset(torch.utils.data.Dataset):
     def __getitem__(self, idx): 
         return self.data[idx]
 
-    def collate_fn(self, batch):
-        videos = []
-        targets = []
-        for i in batch: 
-            videos.append(self.split_video(i)[:,:,:,:2])
-            targets.append(self.split_video(i)[:,:,:,2:])
-        return torch.cat(videos, dim=0).to(torch.float32), torch.cat(targets, dim=0).to(torch.float32)
-        # return torch.cat(videos, dim=0).to(torch.float32), torch.cat(targets, dim=0).to(torch.float32)
+    def create_masked_data(self,data, mask_prob = 0.15):
+        masked_data = data
+        mask = np.random.rand(*masked_data.shape) < mask_prob
+        gaussian_noise = np.random.normal(0, 1, masked_data.shape)
+        masked_data[mask] = gaussian_noise[mask]
+
+        return masked_data
+
+def collate_fn(batch, dataset, mask_prob=0.15):
+    videos = []
+    targets = []
+
+    for i in batch:
+        if dataset.if_train:
+            # Create masked data and split it
+            masked_video = dataset.split_video(dataset.create_masked_data(i, mask_prob))
+            target_video = dataset.split_video(i)
+            videos.append(masked_video[:, :, :, :2])  # Take first two channels
+            targets.append(target_video[:, :, :, :2])  # Same for target
+
+        else:
+            # No masking for inference
+            split_video = dataset.split_video(i)
+            videos.append(split_video[:, :, :, :2])  # First two channels
+            targets.append(split_video[:, :, :, 2:])  # Remaining channels
+
+    # Combine into tensors
+    return (
+        torch.cat(videos, dim=0).to(torch.float32),
+        torch.cat(targets, dim=0).to(torch.float32),
+    )
 
 
 
@@ -60,8 +85,22 @@ class Dataset(torch.utils.data.Dataset):
 # dataset = Dataset(path, 500)
 
 
-# train_loader = torch.utils.data.DataLoader(dataset, batch_size=16, shuffle=True, collate_fn=dataset.collate_fn)
+# # train_loader = torch.utils.data.DataLoader(dataset, batch_size=16, shuffle=True, collate_fn=dataset.collate_fn)
 
-# for i in train_loader: 
-#     print(i)
+# # for vidoes, target in train_loader: 
+# #     print(vidoes.shape, target.shape)
+# #     break
+
+# # dataloader = DataLoader(dataset, collate_fn=lambda batch: collate_fn(batch, dataset))
+# dataloader = TorchDataLoader(
+#     dataset,
+#     batch_size=4,
+#     collate_fn=lambda batch: collate_fn(batch, dataset)
+# )
+
+
+# # Iterate through the DataLoader
+# for videos, targets in dataloader:
+#     print("Videos shape:", videos.shape)
+#     print("Targets shape:", targets.shape)
 #     break
--- source/train.py
+++ source/train.py
@@ -1,15 +1,16 @@
 import torch
-from DataLoaders import Dataset
+from DataLoaders import Dataset, collate_fn
 from DSTFormer import DSTFormer
 from tqdm import tqdm
 from torch.cuda.amp import GradScaler, autocast
 from loss_function import loss_mpjpe
+from torch.utils.data import DataLoader as TorchDataLoader
 scaler = GradScaler()
 
 
 class train: 
     def __init__(self, train_loader, num_epochs): 
-        self.model = DSTFormer(dim_in=2, dim_out=1, embed_size=64, heads=8, max_len=5, num_joints=17, fusion_depth = 3, attn_depth =2, fusion = True)
+        self.model = DSTFormer(dim_in=2, dim_out=2, embed_size=64, heads=8, max_len=5, num_joints=17, fusion_depth = 2, attn_depth =2, fusion = True)
         self.optimizer = torch.optim.AdamW(self.model.parameters(), lr=0.001)
         # self.criterion = torch.nn.MSELoss()
         self.train_loader = train_loader
@@ -20,6 +21,9 @@ class train:
             for i, (video, target) in tqdm(enumerate(self.train_loader)): 
                 self.optimizer.zero_grad()
                 outputs = self.model(video)
+                # print("="*100)
+                # print(outputs.shape, target.shape)
+                # print("="*100)
                 # loss = self.criterion(outputs.to('cuda'), target.to('cuda'))
                 loss = loss_mpjpe(outputs.to('cuda'), target.to('cuda'))
                 scaler.scale(loss).backward()
@@ -27,17 +31,22 @@ class train:
                 scaler.update()
             print(f"Epoch [{epoch+1}/{self.num_epochs}], Loss: {loss.item()}")
 
-            if epoch % 10 == 0: 
+            if epoch % 5 == 0: 
                 torch.save(self.model.state_dict(), f"model_{epoch}.pth")
 
 
 
-batch_size = 256
-num_epochs = 10
+batch_size = 32
+num_epochs = 51
 
 path = '/teamspace/studios/this_studio/data/AMASS/AMASS/amass_joints_h36m_60.pkl'
-dataset = Dataset(path, 1000, max_frames=5)
-train_loader = torch.utils.data.DataLoader(dataset, batch_size=16, shuffle=True, collate_fn=dataset.collate_fn)
+dataset = Dataset(path, 100, max_frames=5, if_train=True)
+# train_loader = torch.utils.data.DataLoader(dataset, batch_size=batch_size, shuffle=True, collate_fn=dataset.collate_fn)
+dataloader = TorchDataLoader(
+    dataset,
+    batch_size=batch_size,
+    collate_fn=lambda batch: collate_fn(batch, dataset)
+)
 
-train = train(train_loader, num_epochs)
+train = train(dataloader, num_epochs)
 train.train()
